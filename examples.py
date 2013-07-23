f720254b4ac93e9b55e3a07c3d77aac656a44008
Ian Ozsvald <ian@ianozsvald.com>
2013-07-23 13:56:21 +0100

    added pytz utc example

--- examples.py
+++ examples.py
@@ -11,3 +11,9 @@ filter_from_str = time.strftime("%Y-%m-%dT%H:%M", filter_from.timetuple())
 print filter_from_str, type(filter_from_str)
 filter_from_new = dt_parser.parse(filter_from_str)
 print filter_from_new, type(filter_from_new)
+
+# now build a GMT datetime string from a web Expires header as GMT
+d = dt_parser.parse('Tue, 23 Jul 2013 12:47:09 GMT')
+print "d as GMT", d  # e.g. 2013-07-23 12:47:09+01:00
+import pytz
+print "d as UTC", d.astimezone(pytz.utc)  # e.g. 2013-07-23 11:47:09+00:00
